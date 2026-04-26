3066baed8ff92d5e300e60c28372974336d5f1d5
julietasegovia <julietavsegovia@gmail.com>
2026-04-26 19:10:14 -0300

    followed the socket.io tutorial, added nickname system

--- index.js
+++ index.js
@@ -2,29 +2,95 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const { availableParallelism } = require('node:os');
+const cluster = require('node:cluster');
+const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
+const { send } = require('express/lib/response');
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i
+    });
+  }
 
-app.get('/', (req, res) => {
+  return setupPrimary();
+}
+
+async function main() {
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+    );
+  `);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
+
+  app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
-});
+  });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+  io.on('connection', async (socket) => {
+
+  socket.on('set nickname', (nickname) => {
+    socket.nickname = nickname;
+    socket.broadcast.emit('message', `${nickname} has joined the chat!`);
+    socket.emit('message', `hii, ${nickname}, you're now able to join the chat :]`);
+  });
+
+  socket.on('chat message', async (msg, clientOffset, callback) => {
+    let result;
+    try {
+      result = await db.run(
+        'INSERT INTO messages (content, client_offset) VALUES (?, ?)',
+        msg, clientOffset
+      );
+    } catch (e) {
+      if (e.errno === 19) callback();
+      return;
+    }
+
+    socket.broadcast.emit('chat message', { text: msg, sender: socket.nickname || 'Anonymous' }, result.lastID);
+    callback(null, result.lastID);
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.nickname)
+      io.emit('message', `${socket.nickname} has left the chat.`);
+  });
+
+  if (!socket.recovered) {
+    try {
+      await db.each(
+        'SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', { text: row.content, sender: 'history' }, row.id);
+        }
+        );
+    } catch (e) {}
+    }
     });
-});
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
-});
+  const port = process.env.PORT;
 
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
-});
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
+
+main();
